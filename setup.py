58e9d8a5c8a10f77a83d5c1618bd4ccc226da5fb
425776024 <425776024@qq.com>
2020-03-07 20:58:43 +0800

    使用描述

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="nlpcda",
-    version="1.0",
+    version="1.2.2",
     keywords=("pip", "nlptool", "nlpcda", "nlp"),
     description="NLP Chinese Data Augmentation，一键中文数据增强工具",
-    long_description="一键中文数据增强工具，支持：随机实体替换，近义词、近义近音字替换，随机字删除。在不改变原文的情况下生成指定数量的训练语料文本",
+    long_description=open('README.rst').read(),
     license="MIT Licence",
 
     url="https://github.com/425776024/nlpcda",
